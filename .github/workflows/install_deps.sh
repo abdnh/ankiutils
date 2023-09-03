c699f193f35dcd5d8995ddc78b22d03642927018
Abdo <abdo@abdnh.net>
2023-09-03 05:19:03 +0300

    Remove pip-tools from dependencies

--- .github/workflows/install_deps.sh
+++ .github/workflows/install_deps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 pip install -r requirements/base.txt
-pip-sync requirements/dev.txt
+pip install -r requirements/dev.txt
 sudo apt install libxcb-xinerama0 libxcb-cursor0 libegl1
