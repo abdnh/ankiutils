ad920f16df7d05ba1f982ddf9e47b4c627b026cb
Abdo <abdo@abdnh.net>
2025-06-20 00:27:52 +0300

    Remove ThreadingIntegration for now

--- src/ankiutils/errors.py
+++ src/ankiutils/errors.py
@@ -22,7 +22,6 @@ from sentry_sdk.integrations.argv import ArgvIntegration
 from sentry_sdk.integrations.dedupe import DedupeIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
 from sentry_sdk.integrations.stdlib import StdlibIntegration
-from sentry_sdk.integrations.threading import ThreadingIntegration
 from sentry_sdk.scope import Scope
 
 from .config import Config
@@ -87,7 +86,7 @@ def _initialize_sentry(args: _ErrorReportingArgs, dsn: str | None = None) -> Non
             DedupeIntegration(),
             LoggingIntegration(),
             StdlibIntegration(),
-            ThreadingIntegration(),
+            # ThreadingIntegration(),
         ],
         # This disable the AtexitIntegration because
         # it causes a RuntimeError when Anki is closed.
